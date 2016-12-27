f4c0d474af3dc68c5b4b69117e997ae4786fa672
xing liu <ogilhinn@gmail.com>
2016-12-27 10:03:17 +0800

    :page_facing_up: create article

--- my-blog/routes/posts.js
+++ my-blog/routes/posts.js
@@ -1,27 +1,84 @@
 let express = require('express');
 let router = express.Router();
+let PostModel = require('../models/posts');
 
 let checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /posts 所有用户或者特定用户的文章页
 //   eg: GET /posts?author=xxx
 router.get('/', function(req, res, next) {
-    res.render('posts');
-});
+    let author = req.query.author;
 
-// POST /posts 发表一篇文章
-router.post('/', checkLogin, function(req, res, next) {
-    res.send(req.flash());
+    PostModel.getPosts(author)
+        .then(function (posts) {
+            res.render('posts', {
+                posts: posts
+            });
+        })
+        .catch(next);
 });
 
 // GET /posts/create 发表文章页
 router.get('/create', checkLogin, function(req, res, next) {
-    res.send(req.flash());
+    res.render('create');
+});
+
+// POST /posts 发表一篇文章
+router.post('/', checkLogin, function(req, res, next) {
+    let author = req.session.user._id;
+    let title = req.fields.title;
+    let content = req.fields.content;
+
+    // 校验参数
+    try {
+        if (!title.length) {
+            throw new Error('请填写标题');
+        }
+        if (!content.length) {
+            throw new Error('请填写内容');
+        }
+    } catch (e) {
+        req.flash('error', e.message);
+        return res.redirect('back');
+    }
+
+    let post = {
+        author: author,
+        title: title,
+        content: content,
+        pv: 0
+    };
+
+    PostModel.create(post)
+        .then(function (result) {
+            // 此 post 是插入 mongodb 后的值，包含 _id
+            post = result.ops[0];
+            req.flash('success', '发表成功');
+            // 发表成功后跳转到该文章页
+            res.redirect(`/posts/${post._id}`);
+        })
+        .catch(next);
 });
 
 // GET /posts/:postId 单独一篇的文章页
 router.get('/:postId', function(req, res, next) {
-    res.send(req.flash());
+    let postId = req.params.postId;
+
+    Promise.all([
+        PostModel.getPostById(postId),// 获取文章信息
+        PostModel.incPv(postId)// pv 加 1
+    ])
+        .then(function (result) {
+            let post = result[0];
+            if (!post) {
+                throw new Error('该文章不存在');
+            }
+
+            res.render('post', {
+                post: post
+            });
+        })
+        .catch(next);
 });
 
 // GET /posts/:postId/edit 更新文章页
