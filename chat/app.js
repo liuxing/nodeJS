20a2a8af917e62b65efab0aa18b2b33a6b94eb9a
xing liu <ogilhinn@gmail.com>
2017-01-12 10:16:32 +0800

    socket.io chat

--- chat/app.js
+++ chat/app.js
@@ -1,63 +1,22 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
-server.listen(3000, function () {
-    console.log('listening on *:3000');
-});
-
-app.get('/', function(req, res){
-    res.send('<h1>Welcome Realtime Server</h1>');
-});
-
-//在线用户
-let onlineUsers = {};
-//当前在线人数
-let onlineCount = 0;
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+})
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+    console.log('a user connected')
+    socket.on('disconnect', function () {
+        console.log('user disconnected')
+    })
+    socket.on('chat message', function (msg) {
+        // console.log('message: ', msg)
+        io.emit('chat message', msg)
+    })
+})
 
-    //监听新用户加入
-    socket.on('login', function(obj){
-        //将新加入用户的唯一标识当作socket的名称，后面退出的时候会用到
-        socket.name = obj.userid;
-
-        //检查在线列表，如果不在里面就加入
-        if(!onlineUsers.hasOwnProperty(obj.userid)) {
-            onlineUsers[obj.userid] = obj.username;
-            //在线人数+1
-            onlineCount++;
-        }
-
-        //向所有客户端广播用户加入
-        io.emit('login', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
-        console.log(obj.username+'加入了聊天室');
-    });
-
-    //监听用户退出
-    socket.on('disconnect', function(){
-        //将退出的用户从在线列表中删除
-        if(onlineUsers.hasOwnProperty(socket.name)) {
-            //退出用户的信息
-            let obj = {userid:socket.name, username:onlineUsers[socket.name]};
-
-            //删除
-            delete onlineUsers[socket.name];
-            //在线人数-1
-            onlineCount--;
-
-            //向所有客户端广播用户退出
-            io.emit('logout', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
-            console.log(obj.username+'退出了聊天室');
-        }
-    });
-
-    //监听用户发布聊天内容
-    socket.on('message', function(obj){
-        //向所有客户端广播发布的消息
-        io.emit('message', obj);
-        console.log(obj.username+'说：'+obj.content);
-    });
-
-});
+http.listen(3000, function(){
+    console.log('listening on *:3000')
+})
