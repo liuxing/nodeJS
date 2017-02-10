744941e5f9ab9c54984fccb2c50d73d26d2fb794
xing liu <ogilhinn@gmail.com>
2017-02-10 16:05:28 +0800

    add delete function

--- manager/bin/manager.js
+++ manager/bin/manager.js
@@ -8,26 +8,66 @@ const DIR = (process.cwd())
 program
     .version(pkg.version)
     // .option('-l, --list', '', fileList)
-    .option('-c, --create', '')
-    .option('-r, --rename', '')
+    .option('-c, --create', '创建文件/文件夹')
+    .option('-d, --delete', '删除文件')     
+    .option('-r, --rename', '文件重命名')
+    .option('-m, --move', '移动文件')
+    .option('-p, --copy', '复制文件') 
     .parse(process.argv)
 
 if(program.create) createFile(program.args[0])
+if(program.delete) delFile(program.args[0])
 if(program.rename) rename(program.args[0], program.args[1])
 
-// 新建文件
+// 新建
 function createFile(name){
-    if(fs.existsSync(name)) return console.log('文件已存在')
-    fs.writeFile(name, '', (err)=>{
-        if(err) throw err
-        console.log('创建成功！')
-    })
+    if(fs.existsSync(name)) return console.log('文件已存在！')
+    // 如果有/则是创建文件夹
+    if(name.indexOf('/') !== -1){
+        fs.mkdir(name, (err)=>{
+            if(err) throw err
+            console.log(`创建${name}文件夹成功！`)
+        })
+    }else{
+        fs.writeFile(name, '', (err)=>{
+            if(err) throw err
+            console.log(`创建${name}成功！`)
+        })
+    }
+}
+
+// 删除
+function delFile(name){
+     if(!fs.existsSync(name)) return console.log('文件不存在！')
+    // 如果有/则是文件夹
+    if(name.indexOf('/') !== -1){
+        fs.rmdir(name, (err)=>{
+            if(err) throw err
+            console.log(`删除${name}文件夹成功！`)
+        })
+    }else{
+        fs.unlink(name, (err)=>{
+            if(err) throw err
+            console.log(`删除${name}成功！`)
+        })
+    }
 }
 
 // 重命名
 function rename(oldname, name){
+    if(!fs.existsSync(oldname)) return console.log('文件不存在！')    
     fs.rename(oldname, name, (err)=>{
         if(err) throw err
         console.log('重名名成功！')
     })
+}
+
+// 移动文件
+function move(name, target){
+    console.log('移动文件')
+}
+
+// 复制文件
+function copy(name, newname){
+    
 }
--- tickets/bin/index.js
+++ tickets/bin/index.js
@@ -4,7 +4,9 @@ const superagent = require('superagent')
 const cliTable = require('cli-table2')
 const program = require('commander')
 const fs =require('fs')
-
+const moment = require('moment')
+moment.locale('zh-cn')
+// 读取12306的证书
 const ca = fs.readFileSync('./config/srca.cer.pem')
 const station = require('./../config/station')
 const API = 'https://kyfw.12306.cn/otn/leftTicket/queryA'
@@ -12,14 +14,30 @@ const API = 'https://kyfw.12306.cn/otn/leftTicket/queryA'
 program
     .parse(process.argv)
 
-let [form, to, data] = program.args
+// 预售期 day
+const DAY = 30
+
+let [form, to, date] = program.args
+let [_form, _to] = [station[form], station[to]]
+let _date = moment(date).isBetween(moment().format('L'), moment().add(DAY, 'days'))
+
+// 判断起始站是否正确
+if(!(_form && _to)){
+    return console.warn('你输入的地点有问题~~~')
+}
+
+// 判断时间
+if(!_date){
+    return console.warn('你选择的时间不在预售范围')
+}
+
 
 superagent
     .get(API)
     .query({
-        'leftTicketDTO.train_date': data,
-        'leftTicketDTO.from_station': station[form],
-        'leftTicketDTO.to_station': station[to],
+        'leftTicketDTO.train_date': date,
+        'leftTicketDTO.from_station': _form,
+        'leftTicketDTO.to_station': _to,
         'purpose_codes': 'ADULT'
     })
     .set('Accept', 'application/json')
@@ -28,6 +46,7 @@ superagent
         if(err){
             return console.error(err)
         }
+        // 终端表格
         let table = new cliTable({
             head: '车次 始末 时间 历时 软座 硬座 软卧 硬卧 无座'.split(' ')
         })
@@ -36,7 +55,7 @@ superagent
             let _item = item['queryLeftNewDTO']
             let line = [
                 _item['station_train_code'],
-                `${_item['from_station_name']}-${_item['to_station_name']}`,
+                `${_item['from_station_name']}->${_item['to_station_name']}`,
                 `${_item['start_time']}-${_item['arrive_time']}`,
                 _item['lishi'],
                 _item['rz_num'],
