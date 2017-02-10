c91ce88f01bcfc264cc07937e81d42f2b1c55f25
xing liu <ogilhinn@gmail.com>
2017-02-10 16:32:15 +0800

    file list,copy

--- manager/bin/manager.js
+++ manager/bin/manager.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const fs = require('fs')
+const path = require('path')
 const program = require('commander')
 const pkg = require('../package')
 // 当前目录
@@ -7,7 +8,7 @@ const DIR = (process.cwd())
 
 program
     .version(pkg.version)
-    // .option('-l, --list', '', fileList)
+    .option('-l, --list', '', fileList)
     .option('-c, --create', '创建文件/文件夹')
     .option('-d, --delete', '删除文件')     
     .option('-r, --rename', '文件重命名')
@@ -18,6 +19,28 @@ program
 if(program.create) createFile(program.args[0])
 if(program.delete) delFile(program.args[0])
 if(program.rename) rename(program.args[0], program.args[1])
+if(program.copy) copy(program.args[0], program.args[1])
+
+// 遍历树 度优先+先序遍历
+function travel(dir, callback){
+    fs.readdirSync(dir).forEach((file)=>{
+        let pathname = path.join(dir, file)
+
+        if(fs.statSync(pathname).isDirectory()){
+            travel(pathname, callback)
+        }else{
+            callback(pathname)
+        }
+    })
+}
+
+
+// 文件列表
+function fileList(){
+    travel(DIR, (pathname)=>{
+        console.log(pathname.replace(DIR+'\\', ''))
+    })
+}
 
 // 新建
 function createFile(name){
@@ -63,11 +86,14 @@ function rename(oldname, name){
 }
 
 // 移动文件
-function move(name, target){
+function move(src, dist){
     console.log('移动文件')
 }
 
 // 复制文件
-function copy(name, newname){
-    
+function copy(src, dist){
+    // @TODO 未实现文件夹拷贝
+    fs.createReadStream(src)
+        .pipe(fs.createWriteStream(dist))
+        .end(console.log('复制完成'))       
 }
